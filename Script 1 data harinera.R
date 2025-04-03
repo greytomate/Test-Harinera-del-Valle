06d88865cb62c928c089d31ba5aa32c66d5617da
greytomate <canorive@gmail.com>
2025-04-03 11:20:24 -0500

    Se va a empezar a procesar la data de varias categorias.

--- Script 1 data harinera.R
+++ Script 1 data harinera.R
@@ -4,9 +4,27 @@ library("dplyr")
 library("ggplot2")
 library("readxl")
 library("Rcpp")
-
+library("tidyverse")
 
 #### Creación de script para limpieza de datos
 
 data <- read_xlsx("C:/Users/Usuario/Desktop/mindlabs/Harinera del Valle/data_harinera.xlsx")
-data <- data[-1,]
+
+
+
+
+
+
+
+
+# ¿Cuáles de los siguientes electrodomésticos tiene en su hogar? (Marque todas las que apliquen)
+
+df_long <- data %>%
+  pivot_longer(cols = c(p6:p11), names_to = "Pregunta", values_to = "Electrodomestico") %>%
+  drop_na()  # Eliminar valores vacíos
+
+# Contar frecuencia de cada electrodoméstico
+conteo <- df_long %>%
+  group_by(Electrodomestico) %>%
+  summarise(Frecuencia = n()) %>%
+  arrange(desc(Frecuencia))
